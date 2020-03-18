ee31b0545beea5f42857413e2b5d03e9e6ec59fa
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-18 14:54:28 -0400

    created web server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hi there');
+const express = require('express');
+
+const app = express();
+
+//Route Handler
+app.get('/', (req, res) => {
+  res.send('Hi there');
+});
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
