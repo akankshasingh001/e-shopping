2f261c92341c43ab4d29bbe48dc932d37a737f10
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-18 16:04:39 -0400

    display simple html page

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app = express();
 
 //Route Handler
 app.get('/', (req, res) => {
-  res.send('Hi there');
+  res.send(`<div>
+  <form>
+  <input placeholder="email"/>
+  <input placeholder="password"/>
+  <input placeholder="password confirmation"/>
+  <button>Sign Up</button>
+  </form>
+  </div>`);
 });
 
 app.listen(3000, () => {
