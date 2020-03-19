5f32d9405845f87e9d99914a5448d329833cc126
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-19 15:56:04 -0400

    added logic for signIn user

--- index.js
+++ index.js
@@ -59,7 +59,18 @@ app.get('/signin', (req, res) => {
   </form>
   </div>`);
 });
-app.post('/signin', async (req, res) => {});
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersRepo.getOneBy({ email });
+  if (!user) {
+    return res.send('Email not found!');
+  }
+  if (user.password !== password) {
+    return res.send('Password incorrect!');
+  }
+  req.session.userId = user.id;
+  res.send('You are signed in!!!');
+});
 app.listen(3000, () => {
   console.log('Listening');
 });
