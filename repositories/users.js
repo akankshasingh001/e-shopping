8336c9673f9beb6c5616feb2404c4c1deda6aa78
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-19 11:06:58 -0400

    opened the repo data file

--- repositories/users.js
+++ repositories/users.js
@@ -2,16 +2,32 @@ const fs = require('fs');
 
 class UsersRepository {
   constructor(filename) {
+    //check if file exist or not
     if (!filename) {
       throw new Error('Creating a repository requires a filename');
     }
     this.filename = filename;
+    //if file exits access with file name otehrwise create a new file
     try {
       fs.accessSync(this.filename);
     } catch (err) {
       fs.writeFileSync(this.filename, '[]');
     }
   }
-  async checkForFile() {}
+  async getAll() {
+    //open the file called this.filename
+    const contents = await fs.promises.readFile(this.filename, {
+      encoding: 'utf8'
+    });
+    //Read its content
+    console.log(contents);
+    //parse the content
+    //Return the parsed data
+  }
 }
-const repo = new UsersRepository('users.json');
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+  await repo.getAll();
+};
+
+test();
