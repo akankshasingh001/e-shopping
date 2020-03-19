203eeb955ce2e3083d0e0b8b97d0c34ab16309cc
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-19 14:01:06 -0400

    added filtered logic for users

--- repositories/users.js
+++ repositories/users.js
@@ -64,10 +64,29 @@ class UsersRepository {
     Object.assign(record, attrs);
     await this.writeAll(records);
   }
+  //filter user
+  async getOneBy(filters) {
+    const records = await this.getAll();
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+      if (found) {
+        return record;
+      }
+    }
+  }
 }
 const test = async () => {
   const repo = new UsersRepository('users.json');
-  await repo.update('9122de16', { password: 'password' });
+  const user = await repo.getOneBy({
+    id: '9122de16'
+  });
+  console.log(user);
 };
 
 test();
