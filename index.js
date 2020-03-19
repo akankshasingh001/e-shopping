231cf870b5d8b34afb443a6c139f3fc7e951e082
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-19 15:43:27 -0400

    added logic for signOut user

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ app.use(
   })
 );
 //Route Handler
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`<div>
+  Your id is : ${req.session.userId}
   <form method="POST">
   <input name="email" placeholder="email"/>
   <input name="password" placeholder="password"/>
@@ -25,7 +26,7 @@ app.get('/', (req, res) => {
   </div>`);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
   const existingUser = await usersRepo.getOneBy({ email });
   if (existingUser) {
@@ -42,6 +43,23 @@ app.post('/', async (req, res) => {
   res.send('Account Created');
 });
 
+//signOut
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out!');
+});
+
+//signIn
+app.get('/signin', (req, res) => {
+  res.send(`<div>
+  <form method="POST">
+  <input name="email" placeholder="email"/>
+  <input name="password" placeholder="password"/>
+  <button>Sign In</button>
+  </form>
+  </div>`);
+});
+app.post('/signin', async (req, res) => {});
 app.listen(3000, () => {
   console.log('Listening');
 });
